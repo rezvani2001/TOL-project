efade922931d521ad7081b6dbf776873f08560fa
Mohammad hosein MHM <MhoseinMola1380@gmail.com>
2021-04-07 21:12:39 +0430

    the methods added but no code in theme yet

--- src/GUI/TransitionEdit.java
+++ src/GUI/TransitionEdit.java
@@ -3,5 +3,31 @@ package GUI;
 import javafx.stage.Stage;
 
 public class TransitionEdit extends Stage {
-    
+    public TransitionEdit() {
+
+    }
+
+    private void makeApplyButton() {
+
+    }
+
+    private void makeMainBody() {
+
+    }
+
+    private void makeNamePart() {
+
+    }
+
+    private void makeLabelPart() {
+
+    }
+
+    private void makeStartStatePart() {
+
+    }
+
+    private void makeEndStatePart() {
+
+    }
 }
