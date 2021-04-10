18c2709aa2926627065b0474b12e51784b451c98
Mohammad hosein MHM <MhoseinMola1380@gmail.com>
2021-04-10 13:49:39 +0430

    the methods for Menu class added

--- src/GUI/Menu.java
+++ src/GUI/Menu.java
@@ -1,7 +1,31 @@
 package GUI;
 
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Menu extends Stage {
-    
+    private VBox mainPane;
+
+    public Menu() {
+
+    }
+
+    public void makeMainBody() {
+    }
+
+    public void makeAddNewAlphabetButton() {
+
+    }
+
+    public void makeAddNewStateButton() {
+
+    }
+
+    public void makeAddNewTransitionButton() {
+
+    }
+
+    public void makeDeleteAlphabetButton() {
+
+    }
 }
