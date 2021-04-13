7ba7070a9769210e008f0a7e42406bab88334658
Mohammad hosein MHM <MhoseinMola1380@gmail.com>
2021-04-13 17:04:26 +0430

    the DeleteAlphabet stage added and the methods added too

---
+++ src/DeleteAlphabet.java
@@ -0,0 +1,23 @@
+import javafx.stage.Stage;
+
+public class DeleteAlphabet extends Stage {
+    public DeleteAlphabet() {
+
+    }
+
+    private void startStage() {
+
+    }
+
+    private void makeAlphabetChooser() {
+        
+    }
+
+    private void makeLabel() {
+
+    }
+
+    private void makeDeleteButton() {
+
+    }
+}
